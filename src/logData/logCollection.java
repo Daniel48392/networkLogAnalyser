e57ec624fd2fc411f0c282daaf7d636bafcec1fa
Daniel48392 <coxhead.dan@gmail.com>
2026-09-25 13:49:12 +0100

    Tweak now that program isn't aiming to read 5000 lines per read and javadoc comments added

--- src/logData/logCollection.java
+++ src/logData/logCollection.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * logCollection class where all the logs are sorted and stored
+ */
 public class logCollection {
     private static List<log> collectionLogs = new ArrayList<>();
 
@@ -20,17 +23,33 @@ public class logCollection {
 
     public static List<String> attackIPs  = new ArrayList<>();
     // contains all instantiated logs
+
+    /**
+     * organizes the CEF logs into lists of threats
+     * <p>
+     *     Uses the logReader class to convert all the CEF text into log classes which then get passed
+     *     into the collectionLogs.
+     *     Then it calls methods in the activityAnalyser class to organize the logs into threats.
+     *     The collectionLogs class attributes which are the lists of each type of threat then get assigned what
+     *     the activityAnalyser's methods determined could be potential threats the logs are now converted to trackers
+     *     specific to each attack.
+     *     The method then sets the threat level for the new Tracker objects and then filters out the IP addresses from
+     *     previous threats so that every already defined threat is not also defined as a DOS attack.
+     * </p>
+     * @param filename - name of file containing CEF logs
+     * @throws IOException - filename cannot be found
+     */
     public static void organiseLogs(String filename) throws IOException {
 
-        while(logReader.endOfFile != true){
-            collectionLogs.addAll(logReader.fileReader(filename));
-            activityAnalyser.injection_SQL_Detection(collectionLogs);
-            activityAnalyser.bruteForceDetector(collectionLogs);
-            activityAnalyser.passwordSprayDetector(collectionLogs);
-            activityAnalyser.portScanVerticalDetector(collectionLogs);
-            activityAnalyser.portScanHorizontalDetector(collectionLogs);
-            activityAnalyser.denialOfServiceDetector(collectionLogs);
-        }
+        collectionLogs.addAll(logReader.fileReader(filename));
+        activityAnalyser.injection_SQL_Detection(collectionLogs);
+        activityAnalyser.bruteForceDetector(collectionLogs);
+        activityAnalyser.passwordSprayDetector(collectionLogs);
+        activityAnalyser.portScanVerticalDetector(collectionLogs);
+        activityAnalyser.portScanHorizontalDetector(collectionLogs);
+        activityAnalyser.denialOfServiceDetector(collectionLogs);
+        collectionLogs.clear();
+
 
         injectionThreats = activityAnalyser.getInjectionThreats();
         bruteForceThreats = activityAnalyser.getBruteForceThreats();
